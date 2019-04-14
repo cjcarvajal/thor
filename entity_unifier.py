e84954b79261296ddd07c524b5062b1b3a52ed63
Carlos Carvajal <ing.carlosj@gmail.com>
2019-04-14 11:10:35 -0500

    Filter tweets for only colombia, add unified entities module

--- entity_unifier.py
+++ entity_unifier.py
@@ -1,9 +1,14 @@
 import twitter_client
+import unified_entities
+import re
+import unidecode
 
 
 def unify_entities_on_tweet(tweet, persisted_accounts):
     tweet.nee_entities = [unify_entity(entity, persisted_accounts)
                           for entity in tweet.nee_entities]
+    tweet.nee_entities = [
+        not_blank_entity for not_blank_entity in tweet.nee_entities if not_blank_entity.text]
     return tweet
 
 
@@ -23,4 +28,25 @@ def unify_entity(entity, persisted_accounts):
                 print(e)
                 print('Entity Unifier')
                 print(twitter_user)
+    else:
+        entity.text = unify_text_from_list(entity.text)
     return entity
+
+
+def unify_text_from_list(entity_text):
+    if entity_text in [normalize_text(false) for false in unified_entities.falses_entities]:
+        return ''
+
+    for pattern in unified_entities.false_entities_pattern:
+        if re.match(pattern.lower(), entity_text.lower()):
+            return ''
+
+    possible_unified_list = [
+        x for x in unified_entities.final_entities if normalize_text(entity_text) in [normalize_text(nick) for nick in x['nicknames']]]
+    if possible_unified_list:
+        return possible_unified_list[0]['realname']
+    return entity_text
+
+
+def normalize_text(text):
+    return unidecode.unidecode(text).lower()
--- thor.py
+++ thor.py
@@ -10,9 +10,3 @@ async def analyze_tweets() -> None:
     tweets = twitter_client.request_tweets('Uribe')
     for tweet in tweets:
         await start_processing.cast(tweet)
-
-'''
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(analyze_tweets())
-'''
